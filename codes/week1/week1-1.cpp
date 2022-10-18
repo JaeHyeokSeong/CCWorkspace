c4b140dba21ae7c1f47535168a49efc2d2386c7b
JaeHyeokSeong <seong5763@gmail.com>
2022-10-19 03:10:46 +1100

    upload week1 codes

--- codes/week1/week1-1.cpp
+++ codes/week1/week1-1.cpp
@@ -2,9 +2,11 @@
 #include <string>
 int main(){
     std::cout << "몇번 출력할것인지 입력해주세요: ";
-    std::string count = "";
-    std::cin >> count;
-    int time = std::stoi(count);
+    // std::string count = "";
+    // std::cin >> count;
+    // int time = std::stoi(count);
+    int time = 0;
+    std::cin >> time;
     for(int i=0; i < time; i++) {
         std::cout << i+1 << ": 성재혁\n";
     }
