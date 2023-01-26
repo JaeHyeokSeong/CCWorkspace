bd2a7245497c71b5427636dbdb0e0a27c5babab1
JaeHyeokSeong <seong5763@gmail.com>
2023-01-27 00:16:19 +1100

    max heap code

--- codes/data_structure/tree/complete_binary_tree/max_heap/max_heap.hpp
+++ codes/data_structure/tree/complete_binary_tree/max_heap/max_heap.hpp
@@ -1,6 +1,85 @@
 #ifndef MAX_HEAP
 #define MAX_HEAP
 
+#include <iostream>
+#include <vector>
 
+int get_parent_index(const int index){
+    return index / 2;
+}
+
+int get_left_child_index(const int index){
+    return index * 2;
+}
+
+int get_right_child_index(const int index){
+    return index * 2 + 1;
+}
+
+class Max_Heap{
+    private:
+        std::vector<int> nodes;
+        int size;
+    public:
+        Max_Heap(){
+            nodes.resize(1);
+            size = 0;
+        }
+        void insert(const int key){
+            nodes.push_back(key);
+            int index = ++size;
+
+            while(index != 1 && nodes[index] > nodes[get_parent_index(index)]){
+                int tmp = nodes[index];
+                nodes[index] = nodes[get_parent_index(index)];
+                nodes[get_parent_index(index)] = tmp;
+                index = get_parent_index(index);
+            }
+        }
+        void remove_max(){
+            if(size == 0){
+                std::cout << "no element to remove\n";
+                return;
+            }
+
+            int tmp = nodes[1];
+            nodes[1] = nodes[size];
+            nodes[size] = tmp;
+
+            nodes.erase(nodes.end() - 1);
+            size--;
+
+            int child = 2;
+            while(child <= size){
+                if(child < size && nodes[child] < nodes[child + 1]) child++;
+
+                if(nodes[get_parent_index(child)] > nodes[child]) break;
+
+                int tmp = nodes[get_parent_index(child)];
+                nodes[get_parent_index(child)] = nodes[child];
+                nodes[child] = tmp;
+
+                child = get_left_child_index(child);
+            }
+        }
+        void peek() const{
+            if(size == 0){
+                std::cout << "no element to peek\n";
+                return;
+            }
+            std::cout << nodes[1] << '\n';
+        }
+        void show() const{
+            if(size == 0){
+                std::cout << "no element to show\n";
+                return;
+            }
+
+            for(int i = 1; i <= size; i++){
+                std::cout << nodes[i] << ' ';
+            }
+            std::cout << '\n';
+        }
+};
 
 #endif // MAX_HEAP
--- codes/data_structure/tree/complete_binary_tree/max_heap/max_heap_practise.cpp
+++ codes/data_structure/tree/complete_binary_tree/max_heap/max_heap_practise.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include "max_heap.hpp"
+
+int read_input(std::string txt){
+    int input;
+    std::cout << txt;
+    std::cin >> input;
+    return input;
+}
+
+void menu(){
+    std::cout << "[0] show, [1] insert, [2] remove_max, [3] peek, [4] exit\n";
+}
+
+void test(){
+    int input = 0;
+    Max_Heap mh;
+    do{
+        menu();
+        input = read_input("enter number: ");
+        if(input == 0) mh.show();
+        else if(input == 1) mh.insert(read_input("enter data: "));
+        else if(input == 2) mh.remove_max();
+        else if(input == 3) mh.peek();
+        else if(input == 4) std::cout << "finished\n";
+    }while(input != 4);
+}
 
 int main(){
-    
+    test();    
     return 0;
 }
